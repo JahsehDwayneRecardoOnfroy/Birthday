5f50d911c72d30536ff79af89550797d57399382
Wolfie <pranjpranjyoti@gmail.com>
2024-02-11 10:03:57 +0530

    hahahahhahaha

--- effect.js
+++ effect.js
@@ -51,13 +51,13 @@ $('document').ready(function(){
 		});
 	});
 
-	function loopOne() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
-			loopOne();
-		});
-	}
+	//function loopOne() {
+		//var randleft = 1000*Math.random();
+		//var randtop = 500*Math.random();
+		//$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
+			//loopOne();
+		//});
+	//}
 	function loopTwo() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
